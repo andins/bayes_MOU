010b2cf499b637006006ef3963eff95f48bd4c9a
andrea <a.insabato@gmail.com>
2018-03-22 16:05:50 +0100

    bla

--- group_meeting/make_rnd_connectivity.py
+++ group_meeting/make_rnd_connectivity.py
@@ -9,7 +9,7 @@ import numpy as np
 
 
 def make_rnd_connectivity(N, density=0.2, connectivity_strength=4.0):
-    C = np.random.rand(N, N)
+    C = np.exp(np.random.randn(N, N))
     C[np.random.rand(N, N) > density] = 0
     C[np.eye(N, dtype=bool)] = 0
     C *= connectivity_strength * N / C.sum()
