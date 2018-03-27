fe3d1954c8ff673be2d8961d347fb1a6f2bdbba9
andrea <a.insabato@gmail.com>
2018-03-27 15:51:10 +0200

    bla

--- group_meeting/estimation_comparison.py
+++ group_meeting/estimation_comparison.py
@@ -12,6 +12,12 @@ import matplotlib.pyplot as plt
 from make_rnd_connectivity import make_rnd_connectivity
 from os import listdir
 from scipy.io import loadmat
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import ShuffleSplit
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+import seaborn as sns
 
 
 # test estimation methods varying N
@@ -211,3 +217,70 @@ for i, T in enumerate(Ts):
 plt.figure()
 plt.errorbar(Ss, rCl.mean(axis=1), rCl.std(axis=1))
 plt.errorbar(Ss, rCm.mean(axis=1), rCm.std(axis=1))
+
+#%% estimate EC from movie dataset with moments and lyapunov method and compare subjects classification
+# load dataset
+ts_movie = np.load('/home/andrea/Work/vicente/data/movie/ts_emp.npy')    
+# remove bad subjects: 1 11 19
+ts_clean = np.delete(ts_movie, [0, 10, 18], 0)
+# load Hagmann SC mask
+movMask = np.load('/home/andrea/Work/vicente/mask_EC.npy')  # [roi, roi] the mask for existing EC connections
+models_l = dict()
+models_m = dict()
+for sb in range(19):  # subjects loop
+    models_l[sb] = dict()
+    models_m[sb] = dict()
+    for ss in range(5):  # sessions loop
+        BOLD_ts = ts_clean[sb, ss, :, :].T
+        models_l[sb][ss] = MOU(n_nodes=66)
+        models_l[sb][ss].fit(X=BOLD_ts, method="lyapunov", SC_mask=movMask)
+        models_m[sb][ss] = MOU(n_nodes=66)
+        models_m[sb][ss].fit(X=BOLD_ts, method="moments", SC_mask=movMask)
+file_name = "MOU_movie.npy"
+np.save(file_name, (models_l, models_m))
+
+# data matrix
+Xl = np.zeros([19*5, np.sum(movMask.flatten())])
+Xm = np.zeros([19*5, np.sum(movMask.flatten())])
+i = 0
+for sb in range(19):  # subjects loop
+    for ss in range(5):  # sessions loop
+        Xl[i, :] = models_l[sb][ss].C[movMask].flatten()
+        Xm[i, :] = models_m[sb][ss].C[movMask].flatten()
+        i += 1
+
+#y = np.array([i for i in range(19) for sess_id in range(5)])  # labels
+y = np.array([0 if sess_id<2 else 1 for i in range(19) for sess_id in range(5)])  # labels
+
+clf = LogisticRegression(C=10000, penalty='l2', multi_class= 'multinomial', solver='lbfgs')
+
+# corresponding pipeline: zscore and pca can be easily turned on or off
+pipe_l = Pipeline([('zscore', StandardScaler()),
+                         ('clf', clf)])
+pipe_m = Pipeline([('zscore', StandardScaler()),
+                         ('clf', clf)])
+repetitions = 100  # number of times the train/test split is repeated
+# shuffle splits for validation test accuracy
+shS = ShuffleSplit(n_splits=repetitions, test_size=None, train_size=.8, random_state=0)
+
+score_l = np.zeros([repetitions])
+score_m = np.zeros([repetitions])
+
+i = 0  # counter for repetitions
+for train_idx, test_idx in shS.split(Xl):  # repetitions loop
+    data_trainl = Xl[train_idx, :]
+    data_trainm = Xm[train_idx, :]
+    y_train = y[train_idx]
+    data_testl = Xl[test_idx, :]
+    data_testm = Xm[test_idx, :]
+    y_test = y[test_idx]
+    pipe_l.fit(data_trainl, y_train)
+    pipe_m.fit(data_trainm, y_train)
+    score_l[i] = pipe_l.score(data_testl, y_test)
+    score_m[i] = pipe_m.score(data_testm, y_test)
+    i+=1
+        
+# plot comparison as violin plots
+fig, ax = plt.subplots()
+sns.violinplot(data=[score_l, score_m], cut=0, orient='v', scale='width')
+ax.set_xticklabels(['lyapunov', 'moments'])
